bd5ff3bcf9cafc6698626d41cfb350ed80403b62
John Balderson <38844243+johnbalderson@users.noreply.github.com>
2022-12-15 06:53:01 -0500

    Final submission to Udacity 12/15/22

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -10,25 +10,38 @@ import java.util.List;
 
 public class AdminResource {
 
+    // create singleton for AdminResource
+    private static final AdminResource SINGLETON = new AdminResource();
+
+    private AdminResource() {}
+
+    public static AdminResource getSingleton() {
+        return SINGLETON;
+    }
+
+    // get singletons for Customer Service and Reservation Service
     private static final CustomerService customerService = CustomerService.getSingleton();
     private static final ReservationService reservationService = ReservationService.getSingleton();
 
-    public static Customer getCustomer(String email){
-        return customerService.getCustomer(email);
-    }
 
+    // add a room
     public static void addRoom(List<IRoom> rooms){
         for (IRoom room:rooms) {reservationService.addRoom(room);}
     }
 
+    // get a collection of a list of reservations
     public static Collection<IRoom> getAllRooms(){
-        return reservationService.getAllRooms();
+        return reservationService.getRoomList().values();
     }
 
+    // get a collection of all customers
     public static Collection<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
+
+    // display all reservations
     public static void displayAllReservations() {
         reservationService.printAllReservation();
     }
+
 }
