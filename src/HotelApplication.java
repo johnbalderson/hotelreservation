40e3d4cdb998987fbe0af041cad7ff67d299cc49
John Balderson <38844243+johnbalderson@users.noreply.github.com>
2022-12-15 06:56:41 -0500

    Final submission to Udacity 12/15/22
    Main app and tester app

--- src/HotelApplication.java
+++ src/HotelApplication.java
@@ -2,6 +2,6 @@ import menu.MainMenu;
 
 public class HotelApplication {
     public static void main(String[] args) throws Exception {
-          MainMenu.runMainApp();
+        MainMenu.runMainApp();
     }
 }
